85be61b2ea50f7f33be837c85debe10930ffb420
Ying Ruei Liang (KK) <thumbd03803@gmail.com>
2015-08-06 09:49:09 +0800

    Now with minimize ability to do simple ordering

--- main.py
+++ main.py
@@ -19,6 +19,7 @@
 
 from pprint import pprint
 import sys
+import time
 import random
 import re
 import argparse
@@ -29,18 +30,16 @@ BENDON_SITE = "https://dinbendon.net"
 
 username = "guest"
 password = "guest"
-username_for_ordering = "I'm a robot from outerspace"
 
 class Menu(object):
     def __init__(self, session, url):
         self.menu_ = {}
-        self.payload = {}
+        self.userNameForOrdering = ""
 
         id = session.cookies.get('JSESSIONID')
         orderReq = session.get(url)
         soup = BeautifulSoup(orderReq.text, 'lxml')
-        self.menu_['names'] = []
-        self.menu_['prices'] = []
+        self.menu_['items'] = []
         self.menu_['qtyInputs'] = []
         self.menu_['commentInputs'] = []
         self.menu_['userInput'] = soup.find('table', 'lists').find('input')['name']
@@ -58,44 +57,46 @@ class Menu(object):
                 for label in i.find('td', 'variationPrice').find_all('label'):
                     price = price.join(label.string + ' ')
 
+            item = {
+                     "name": name,
+                     "price": price
+                   }
             qtyInput = {
                          "name": qtyInputName,
                          "qty" : ''
                        }
-
             commentInput = {
                              "name": commentInputName,
                              "comment": ""
                            }
 
-            self.menu_['names'].append(name)
-            self.menu_['prices'].append(price)
+            self.menu_['items'].append(item)
             self.menu_['qtyInputs'].append(qtyInput)
             self.menu_['commentInputs'].append(commentInput)
 
-    def getItemNameList(self):
-        return self.menu_["names"]
+    def getItemList(self):
+        return self.menu_["items"]
 
-    def getItemPriceList(self):
-        return self.menu_["prices"]
-
-    def setItemQty(self, itemName, qty):
+    def setItemQty(self, item, qty):
         if type(qty) is not type(666):
             print(type(qty))
             raise Exception("input qty is not number")
 
-        index = self.menu_["names"].index(itemName)
+        index = self.menu_["items"].index(item)
         self.menu_['qtyInputs'][index]["qty"] = str(qty)
 
-    def setItemComment(self, itemName, comment):
-        index = self.menu_["names"].index(itemName)
+    def setItemComment(self, item, comment):
+        index = self.menu_["items"].index(item)
         self.menu_['commentInputs'][index]["comment"] = str(comment)
+
+    def setNameForOrdering(self, name):
+        self.userNameForOrdering = name
 
     def sendOrder(self, session):
         urlToPost = self.menu_['urlToPost']
 
         payload = {}
-        payload[self.menu_['userInput']] = username_for_ordering
+        payload[self.menu_['userInput']] = self.userNameForOrdering
         payload['addOrderItemForm:hf:0'] = ""
 
         for qtyInput in self.menu_["qtyInputs"]:
@@ -106,64 +107,109 @@ class Menu(object):
 
         session.post(urlToPost, data=payload)
 
-def getLoginPostUrl(session):
-    return (BENDON_SITE + "/do/?wicket:interface=:0:signInPanel:signInForm::IFormSubmitListener")
+class BenDonSession(object):
+    def __init__(self, username, password):
+        self.session = requests.Session()
 
-def getOrderUrl(session):
-    r = session.get(BENDON_SITE + "/do")
-    soup = BeautifulSoup(r.text, 'lxml')
-    urlsToReturn = []
+        if not self.login_(username, password):
+            raise Exception("BenDonSession initialization falied")
 
-    for dom in soup.find_all('tr', id=re.compile('inProgressBox_inProgressOrders_\d+')):
-        AllA = dom.find_all('a')
-        if not len(AllA) == 4:
-            continue
+    def getInProgressOrderings(self):
+        r = self.session.get(BENDON_SITE + "/do")
+        soup = BeautifulSoup(r.text, 'lxml')
+        urlsToReturn = []
 
-        href = AllA[3]['href']
-        urlsToReturn.append(BENDON_SITE + href)
+        for dom in soup.find_all('tr', id=re.compile('inProgressBox_inProgressOrders_\d+')):
+            AllA = dom.find_all('a')
+            if not len(AllA) == 4:
+                continue
 
-    return urlsToReturn
+            creator = AllA[2].find_all('span')[0].string
+            shopName = AllA[2].find_all('span')[1].string
+            count = AllA[0].find('span').string
+            detailUrl = BENDON_SITE + AllA[2]['href']
+            orderUrl = BENDON_SITE + AllA[3]['href']
 
-def login(session):
-    r = session.get(BENDON_SITE + "/do/login")
-    soup = BeautifulSoup(r.text, 'lxml')
+            inProgressOrdering = {
+                                   "creator": creator,
+                                   "shopName": shopName,
+                                   "count": count,
+                                   "detailUrl": detailUrl,
+                                   "orderUrl": orderUrl
+                                 }
 
-    # Since cpacha here is the form of '1+49=', we can easily get result
-    capcha = soup.select('td')[6].text.rstrip('=')
-    exec('result = %s' % capcha)
+            urlsToReturn.append(inProgressOrdering)
 
-    data = {
-            "username":username,
-            "password":password,
-            "result":str(result),
-            "submit":"login",
-            "rememberMeRow:rememberMe":"on",
-            "signInPanel_signInForm:hf:0":""
-    }
+        return urlsToReturn
 
-    urlToPost = getLoginPostUrl(session)
-    session.post(urlToPost, data=data)
+    def login_(self, username, password):
+        r = self.session.get(BENDON_SITE + "/do/login")
+        soup = BeautifulSoup(r.text, 'lxml')
+        urlToPost = BENDON_SITE + soup.find('form', id='signInPanel_signInForm')['action']
 
-    return session.cookies.get('INDIVIDUAL_KEY')
+        # Since cpacha here is the form of '1+49=', we can easily get result
+        capcha = soup.select('td')[6].text.rstrip('=')
+        exec('result = %s' % capcha)
+
+        data = {
+                "username":username,
+                "password":password,
+                "result":str(result),
+                "submit":"login",
+                "rememberMeRow:rememberMe":"on",
+                "signInPanel_signInForm:hf:0":""
+        }
+
+        self.session.post(urlToPost, data=data)
+
+        return self.session.cookies.get('INDIVIDUAL_KEY')
 
 def cli(args):
-    s = requests.Session()
+    benDon = BenDonSession(username, password)
 
-    login(s)
+    orderings =  benDon.getInProgressOrderings()
 
-    r = s.get(BENDON_SITE + "/do")
-    soup = BeautifulSoup(r.text, 'lxml')
+    # Choose which ordering to order
+    for i in range(0, 5):
+        ordering = orderings[i]
+        creator = ordering["creator"]
+        shopName = ordering["shopName"]
+        detailUrl = ordering["detailUrl"]
+        orderUrl = ordering["orderUrl"]
+        count = ordering["count"]
 
-    for orderUrl in getOrderUrl(s):
-        menu = Menu(s, orderUrl)
-        itemCount = len(menu.getItemNameList())
-        itemIndexToOrder = random.randrange(0, itemCount)
-        itemNameToOrder = menu.getItemNameList()[itemIndexToOrder]
-        menu.setItemQty(itemNameToOrder, 9)
-        menu.setItemComment(itemNameToOrder, "You can run but you can't hide")
-        menu.sendOrder(s)
-        print("You have ordered \'%s\' as \'%s\'!" % (itemNameToOrder, username_for_ordering))
-        break
+        print("[%d] %s create %s, now %s people has ordered"
+              % (i, creator, shopName, count))
+
+    chosenOrdering = int(raw_input('which to order?'))
+
+    # Choose which item you want for lunch
+    menu = Menu(benDon.session, orderings[chosenOrdering]["orderUrl"])
+    items = menu.getItemList()
+    itemCount = len(items)
+
+    for i in range(0, itemCount):
+        item = items[i]
+
+        print("[%d] %s\tprice: %s" % (i, item["name"], item["price"]))
+
+    choosenItem = int(raw_input('which to order? '))
+    orderQty = int(raw_input('quantity to order? '))
+    orderComment = raw_input('any comment? (default is nothing) ')
+    username_for_ordering = ""
+    while username_for_ordering is "":
+        username_for_ordering = raw_input("name for ordering? ")
+
+    # Give me your personal information for free
+    itemToOrder = items[choosenItem]
+    menu.setNameForOrdering(username_for_ordering)
+    menu.setItemQty(itemToOrder, orderQty)
+    menu.setItemComment(itemToOrder, orderComment)
+    menu.sendOrder(benDon.session)
+    print("You have ordered \'%s\' from \'%s\' as \'%s\'!"
+          % (itemToOrder["name"], orderings[chosenOrdering]["shopName"], username_for_ordering))
+
+    return 0
 
 if __name__ == "__main__":
     exit(cli(sys.argv[1:]))
